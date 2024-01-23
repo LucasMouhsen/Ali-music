634e3acd31324b3216fae09fce8345b21dbda3b1
lucasmouhsen <lucasmouhsen@gmail.com>
2024-01-23 14:41:37 -0300

    Rutas actualizadas

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const {products, createProduct, editProduct, deleteProduct} = require('../controllers/productsController')
+const { products, createProduct, editProduct, deleteProduct } = require('../controllers/productsController')
+const uploadProduct = require('../middleware/fotoProductMulter')
 
 const loginCheck = require("../middleware/loginCheck");
 const productValidation = require('../validations/productValidation');
+const editProductValidation = require('../validations/editProductValidation');
 /* GET home page. */
 
 /* router.get('/cart', loginCheck, cart);
@@ -12,15 +14,9 @@ router.put('/cart/:id', loginCheck, addCart) */
 
 
 router.get('/', products)
-router.post('/create',(req, res) =>{
-    
-  res.header('Access-Control-Allow-Origin', '');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.status(204).end();
-}, productValidation, createProduct)
-router.post('/edit', editProduct)
-router.post('/delete', deleteProduct) 
+router.post('/create', loginCheck,   uploadProduct.array('image'), productValidation, createProduct)
+router.post('/edit/:id', loginCheck, uploadProduct.array('image'), productValidation, editProduct)
+router.post('/delete/:id', loginCheck, deleteProduct)
 
 
 
