7a0f7bf1070e50b2b10442ca8d6e87ee890b995c
lucasmouhsen <lucasmouhsen@gmail.com>
2024-02-05 16:04:32 -0300

    test y production quitaods

--- backend/src/database/config/config.js
+++ backend/src/database/config/config.js
@@ -8,19 +8,5 @@ module.exports={
     "host": process.env.DB_HOST,
     "dialect": "mysql",
     "port": process.env.DB_PORT,
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
   }
 }
