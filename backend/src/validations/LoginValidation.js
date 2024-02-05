5518d9831d7a45cdd5264ff81ddd2cbbf2e02083
lucasmouhsen <lucasmouhsen@gmail.com>
2024-02-05 16:02:56 -0300

    cambio Validacion de mail

--- backend/src/validations/LoginValidation.js
+++ backend/src/validations/LoginValidation.js
@@ -6,30 +6,20 @@ const db = require('../database/models')
 module.exports = [
      check('email')
     .isEmail().withMessage('Debe ingresar un email ').bail()
-    .custom((value,{req}) => {
-        
-        return db.User.findOne({
-            where : {
-                email : value,
-            }
-        })
-            .then(user => {
-                if(!user){
-                    return Promise.resolve()
-                }
-            }).catch( () => Promise.reject('Credenciales inválidas'))
-    }),
-    
+    ,
     check('password')
     .notEmpty().withMessage('El campo de contraseña no puede estar vacío')
     .custom((value, { req }) => {
         return db.User.findOne({
             where: {
-                email: req.body.email,  // Accede a req.body en lugar de req.query
+                email: req.body.email, 
             }
         })
             .then(User => {
-                if (!bcrypt.compareSync(value, User.password)) {  // Usar el valor pasado al middleware
+                if(!User){
+                    return Promise.reject('Credenciales inválidas');
+                }
+                if (!bcrypt.compareSync(value, User.password)) { 
                     return Promise.reject('Credenciales inválidas');  // Simplifica el mensaje de error
                 }
             }).catch(() => Promise.reject('Credenciales inválidas'));
