b6e4aa4b39d916ec70c3cb78b070e4668d2b57cd
Lucas12github <Lucasmouhsen@gmail.com>
2024-02-05 23:43:47 -0300

    provider de categorias, cart y modal agregados

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import AppRoutes from './routes'
 import './app.css'
 import { ProductProvider } from './context/productsProvider'
+import { CategoriesProvider } from './context/categoriesProvider'
+import { ModalProvider } from './context/ModalProvider'
+import { CartProvider } from './context/cartProvider'
 function App() {
   return (
-    <ProductProvider>
-      <AppRoutes />
-    </ProductProvider>
+    <ModalProvider>
+      <CartProvider>
+        <ProductProvider>
+          <CategoriesProvider>
+            <AppRoutes />
+          </CategoriesProvider>
+        </ProductProvider>
+      </CartProvider>
+    </ModalProvider>
   )
 }
 
