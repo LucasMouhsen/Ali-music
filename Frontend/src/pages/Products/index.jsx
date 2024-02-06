2d91f0bd108126d3dc50d50decfe4884873ff5c2
Lucas12github <Lucasmouhsen@gmail.com>
2024-02-05 23:46:50 -0300

    Nuevo renderizado condicional

--- Frontend/src/pages/Products/index.jsx
+++ Frontend/src/pages/Products/index.jsx
@@ -3,20 +3,30 @@ import Header from "../../components/Header";
 import CardProducts from "../../components/CardProduct";
 import useProducts from '../../hooks/useProducts'
 import styles from './index.module.css'
+import SlideSearch from '../../components/SlideSearch';
+import CardProductsEmpty from '../../components/CardProductEmpty';
+import CartModal from '../../components/CartProduct';
 
 export default function Products() {
-    const {products} = useProducts()
-    
+    const { products, loading } = useProducts()
+
     return (
         <>
             <Header />
-            <section className={styles.productsList}>
-                {products && products.map((product) => {
-                    return (
-                        <CardProducts key={product.id} product={product} />
-                    )
-                })}
-            </section>
+            <CartModal/>
+            <main className={styles.products}>
+                <SlideSearch />
+                <section className={styles.productsList}>
+                    {loading ?
+                        <CardProductsEmpty/> :
+                        products && products.map(product => {
+                            return (
+                                <CardProducts key={product.id} product={product} />
+                            )
+                        })
+                    }
+                </section>
+            </main>
         </>
     );
 }
