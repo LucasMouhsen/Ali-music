a084ea537db876751689409c79c99871e14a7dec
lucasmouhsen <lucasmouhsen@gmail.com>
2024-02-07 15:59:55 -0300

    Nuevo Formulario login

--- Frontend/src/pages/Login/index.jsx
+++ Frontend/src/pages/Login/index.jsx
@@ -1,10 +1,19 @@
 import Header from "../../components/Header";
-
+import styles from './index.module.css'
 export default function Login() {
     return (
         <>
-            <Header />
-            <h1>Login</h1>
+            {/* <Header /> */}
+            <section className={styles.login}>
+                <div className={styles.container}>
+                    <form className={styles.form} action="">
+                        <p className={styles.title}>Login Form</p>
+                        <input placeholder="Email" className={`${styles.email} ${styles.input}`} type="text" />
+                        <input placeholder="Password" className={`${styles.password} ${styles.input}`} type="password" />
+                        <button className={styles.btn} type="submit">Login</button>
+                    </form>
+                </div>
+            </section>
         </>
     )
 }
