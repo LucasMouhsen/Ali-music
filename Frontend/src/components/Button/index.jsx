c5f6d2e391d6e73400d57e2d0ed994124cc4f9a4
Lucas12github <Lucasmouhsen@gmail.com>
2024-02-09 15:55:48 -0300

    Cambio de funcion de prop

--- Frontend/src/components/Button/index.jsx
+++ Frontend/src/components/Button/index.jsx
@@ -1,7 +1,7 @@
 import styles from './index.module.css'
 export default function Button({ text, href }) {
     return (
-        <button className={styles.button} onClick={() => window.location.href = href}> {text}
+        <button className={styles.button} onClick={href}> {text}
         </button>
     )
 }
