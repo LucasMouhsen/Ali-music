13878f1a4c417b3399fcf37e0d38a005e6abeb44
Lucas12github <Lucasmouhsen@gmail.com>
2024-02-12 00:21:47 -0300

    Nuevo muestreo de productos en perfil

--- Frontend/src/pages/Profile/index.jsx
+++ Frontend/src/pages/Profile/index.jsx
@@ -1,43 +1,67 @@
+import { useEffect, useState } from "react";
 import CardProduct from "../../components/CardProduct";
 import Header from "../../components/Header";
 import useProducts from "../../hooks/useProducts";
 import useUser from "../../hooks/useUser";
 import styles from './index.module.css'
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 export default function Profile() {
-    const { user, loading } = useUser()
-    const { products } = useProducts()
+    const { user, loading: loadingUser } = useUser()
+    const { products, loading: loadingProducts } = useProducts()
+    const [productUser, setProductUser] = useState([]);
+
+    useEffect(() => {
+        if (user && products && products.length > 0) {
+            const productUserFilter = products.filter((product) => product.userId === user.id)
+            setProductUser(productUserFilter)
+        }
+    }, [products, user])
     return (
         <>
             <Header />
             {
-                loading ?
+                loadingUser && loadingProducts ?
                     <h1>Cargando</h1> :
                     <div className={styles.boxProfile}>
                         <div className={styles.boxInfo}>
-                            <h2 className={styles.title}>Mi perfil</h2>
+                            <div className={styles.boxTitle}>
+                                <h2 className={styles.title} >Mi perfil</h2>
+                                <FontAwesomeIcon className={styles.icon} icon={faEdit} />
+                            </div>
                             <div className={styles.boxImage}>
                                 <img className={styles.image} src={`images/fotoUser/${user?.avatar}`}></img>
                             </div>
                             <div className={styles.boxDataUser}>
-                                <p className={styles.dataUser}>Usuario: <span>{user?.userName}</span></p>
-                                <p className={styles.dataUser}>Nombre: <span>{user?.firstName + ' ' + user?.lastName}</span></p>
-                                <p className={styles.dataUser}>Teléfono: <span>{user?.number != null ? 'no hay' : user?.number}</span></p>
+                                <p className={styles.dataUser}>Usuario: <span>{user?.userName} </span></p>
+                                <p className={styles.dataUser}>Nombre: <span>{user?.firstName} </span></p>
+                                <p className={styles.dataUser}>Apellido: <span>{user?.lastName} </span></p>
+                                <p className={styles.dataUser}>Teléfono: <span>{user?.number} </span></p>
                                 <p className={styles.dataUser}>Email: <span> {user?.email} </span></p>
                             </div>
                         </div>
-                        <div className={styles.boxCart}>
-                            <h2 className={styles.title}>Compras realizadas</h2>
-                        </div>
                         <div className={styles.boxProducts}>
-                            <h2 className={styles.title}>Mis productos</h2>
+                            <div className={styles.boxTitle}>
+                                <h2 className={styles.title} >Mis productos</h2>
+                                <FontAwesomeIcon className={styles.icon} icon={faInfoCircle} />
+                            </div>
                             <div className={styles.boxProduct}>
-                                {products && products.map((product) => {
-                                    if (product.userId === user.id) {
-                                        return (
-                                            <CardProduct key={product.id} product={product} detail={false} />
-                                        )
-                                    }
-                                })}
+                                {
+                                    productUser && productUser.slice(0, 3).map((product) => {
+                                        if (product.userId === user.id) {
+                                            return (
+                                                <CardProduct key={product.id} product={product} detail={false} />
+                                            )
+                                        }
+                                    })
+                                }
+                            </div>
+                        </div>
+                        <div className={styles.boxCart}>
+                        <div className={styles.boxTitle}>
+                                <h2 className={styles.title} >Compras realizadas</h2>
+                                <FontAwesomeIcon className={styles.icon} icon={faInfoCircle} />
                             </div>
                         </div>
                     </div>
